eca1cd962dd0072bce19bc728f4d1ca3655334ca
discphy <dinophy@nate.com>
2024-11-26 08:51:54 +0900

    :sparkles: 코틀린 컬렉션을 함수형으로 다루는 법

---
+++ src/main/java/com/lannstark/lec17/Lec17Main.kt
@@ -0,0 +1,34 @@
+package com.lannstark.lec17
+
+import java.util.*
+
+fun main() {
+    val fruits = listOf(
+        Fruit("사과", 1000),
+        Fruit("사과", 1200),
+        Fruit("사과", 1200),
+        Fruit("사과", 1500),
+        Fruit("바나나", 3000),
+        Fruit("바나나", 3200),
+        Fruit("바나나", 2500),
+        Fruit("수박", 10000)
+    )
+
+    var isApple = fun(fruit: Fruit): Boolean {
+        return fruit.name == "사과"
+    }
+
+    val isApple2 = { fruit: Fruit -> fruit.name == "사과" }
+
+    filterFruits(fruits) { it.name == " 사과" }
+}
+
+private fun filterFruits(fruits: List<Fruit>, filter: (Fruit) -> Boolean): List<Fruit> {
+    val results = mutableListOf<Fruit>()
+    for (fruit in fruits) {
+        if (filter.invoke(fruit)) {
+            results.add(fruit)
+        }
+    }
+    return results
+}
